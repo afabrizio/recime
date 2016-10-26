d0b96ff41001b8239f848b464000125bc30c0dc1
afabrizio <andrew.m.fabrizio@gmail.com>
2016-10-26 13:43:41 -0700

    MongoDB had failed during deployment. Modigied index.js to accomodate development and production scenarios.

--- index.js
+++ index.js
@@ -3,21 +3,27 @@ const express  = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes.js');
 
+const DEFAULT_MONGO_URI = 'mongodb://localhost:27017/recime';
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const MONGODB_URI = process.env.MONGODB_URI || DEFAULT_MONGO_URI;
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
-MongoClient.connect('mongodb://localhost:27017/recime', (err, db) => {
+MongoClient.connect(MONGODB_URI, (err, db) => {
   if(err) {
     console.error(err);
     process.exit(1);
   }
 
-  app.set('port', (process.env.PORT || 5000));
+  app.set('port', PORT);
 
   app.use('/', express.static(__dirname + '/dist/public') );
   app.use(bodyParser.json());
   app.use(routes(db));
 
-  app.listen( (process.env.PORT || 5000), function() {
-      console.log('Node app is running on port', (process.env.PORT || 5000) );
+  app.listen( PORT, function() {
+      NODE_ENV !== 'production' &&
+      console.log('Node app is running on port', PORT );
   });
 });
