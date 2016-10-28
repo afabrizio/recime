185322a2323d10869c48746351f3c864f0cc25e6
Andrew Fabrizio <andrew.m.fabrizio@gmail.com>
2016-10-28 10:34:26 -0700

    Updated a few configuration settings and details.

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,23 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: './index.js',
-
   output: {
     path: 'public',
     filename: 'bundle.js',
     publicPath: '/'
   },
-
+  devtool: 'source-map',
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+      {
+        test: /\.js$/,
+        loader: 'babel-loader?presets[]=es2015&presets[]=react',
+        exclude: /node_modules/
+      }
     ]
   },
 
-  plugins: process.env.NODE_ENV === 'prodiction' ? [
+  plugins: process.env.NODE_ENV === 'production' ? [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin()
